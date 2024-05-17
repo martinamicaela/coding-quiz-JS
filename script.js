d4ed4ce388ea876a43fd681916c2503c80dbf334
martinamicaela frutos <martinamicaela924@gmail.com>
2024-05-17 10:38:18 +0000

    create const vairables for js and let variables for js

--- script.js
+++ script.js
@@ -180,3 +180,11 @@ const questions = [
         ]
     }
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
